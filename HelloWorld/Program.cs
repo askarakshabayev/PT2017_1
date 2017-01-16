2382fcf0b764e0e31a418d54150fcc069cacb5b0
Askar <askar.akshabayev@gmail.com>
2017-01-16 11:18:50 +0600

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// Main function
+        /// </summary>
+        /// <param name="args"> arguments </param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");    
+        }
+    }
+}
